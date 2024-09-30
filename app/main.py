99b9edade1518fd27ad7c8e2f49c5cf8b632d8c8
shukawam <shukawam@gmail.com>
2024-09-30 19:47:17 +0900

    Added reranker

--- app/main.py
+++ app/main.py
@@ -6,8 +6,10 @@ import streamlit as st
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain.retrievers.contextual_compression import ContextualCompressionRetriever
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_cohere.chat_models import ChatCohere
+from langchain_cohere.rerank import CohereRerank
 from langfuse import Langfuse
 from langfuse.callback import CallbackHandler
 
@@ -58,8 +60,10 @@ OpenAI GPT-4o, Cohere Command R+のどちらかを選んで利用可能です。
 # サイドバー関連
 with st.sidebar.container():
     with st.sidebar:
+        st.sidebar.markdown("### LLM関連パラメータ")
         model_name = st.sidebar.selectbox(
-            label="Model Name", options=["command-r-plus", "gpt-4o-mini"]
+            label="Model Name",
+            options=["command-r-plus", "gpt-4o-mini"],
         )
         max_tokens = st.sidebar.slider(
             label="Max Tokens",
@@ -77,13 +81,28 @@ with st.sidebar.container():
             step=0.1,
             help="モデルの出力のランダム性",
         )
-
-# Vector Storeの初期化
-if "vector_store" not in st.session_state:
-    st.session_state["vector_store"] = vs.initialize(
-        model_name=model_name, session_id=session_id
-    )
-vector_store = st.session_state["vector_store"]
+        st.sidebar.markdown("### 検索関連パラメータ")
+        top_k = st.sidebar.slider(
+            label="Top K",
+            min_value=1,
+            max_value=10,
+            value=5,
+            step=1,
+            help="関連情報の取得数",
+        )
+        use_reranker = st.sidebar.radio(
+            label="Reranker",
+            options=[True, False],
+            horizontal=True,
+            help="Rerankerを使用するか（※使用には、CohereのAPI Keyが必要です）",
+        )
+        top_n = st.sidebar.slider(
+            label="Top N",
+            min_value=1,
+            max_value=10,
+            value=3,
+            help="Rerankerで取得した情報を何件に絞り込むか",
+        )
 
 
 def generate_response(query: str):
@@ -98,19 +117,34 @@ def generate_response(query: str):
     elif model_name == "command-r-plus":
         chat_model = ChatCohere(
             cohere_api_key=cohere_api_key,
-            model_name=model_name,
+            model=model_name,
             temperature=temperature,
             max_tokens=max_tokens,
         )
     else:
         logger.error("Unsetted model name")
 
+    # 軽微な処理なので、アプリケーションの実行ごとに初期化する
+    vector_store = vs.initialize(model_name=model_name, session_id=session_id)
+    retriever = vector_store.as_retriever(search_kwargs={"k": top_k})
+
+    # Rerankerの使用フラグが有効（デフォルト）の場合は、CohereのRerankerを用いて、
+    # 取得した情報を関連度順に並び替えた後に、指定件数分のみ採用する
+    if use_reranker == True:
+        compressor = CohereRerank(
+            cohere_api_key=cohere_api_key, model="rerank-multilingual-v3.0", top_n=top_n
+        )
+        retriever = ContextualCompressionRetriever(
+            base_compressor=compressor, base_retriever=retriever
+        )
+
     # bbql-app-promptをLangfuse上で作成すると、回答が変わることを確認する
     prompt = langfuse.get_prompt(
         name="bbql-app-prompt", fallback=fallback_prompt
     ).get_langchain_prompt()
+
     chain = (
-        {"question": RunnablePassthrough(), "context": vector_store.as_retriever()}
+        {"question": RunnablePassthrough(), "context": retriever}
         | PromptTemplate.from_template(prompt)
         | chat_model
         | StrOutputParser()
